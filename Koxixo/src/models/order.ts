b464ce291472721055f22bd2839b6c7b8166d070
Italo Marlisson Ferreira da Silva <italomarlissonf@gmail.com>
2025-09-05 20:40:06 -0300

    Koxixo

--- Koxixo/src/models/order.ts
+++
@@ -1,9 +0,0 @@
-export interface Order {
-    id: string;
-    userId: string;
-    status: 'pending' | 'completed' | 'canceled';
-    history: Array<{
-        status: string;
-        timestamp: Date;
-    }>;
-}
